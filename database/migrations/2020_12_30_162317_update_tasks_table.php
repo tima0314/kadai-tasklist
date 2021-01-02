b8b483f0b0e1911d3af38b73cab883ee7561b856
tima0314 <tima314@gmail.com>
2021-01-02 00:53:18 +0000

    CRUD tasks

--- database/migrations/2020_12_30_162317_update_tasks_table.php
+++ database/migrations/2020_12_30_162317_update_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class UpdateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-
-            $table->string('content');    // contentカラム追加
-            $table->timestamps();
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+           
         });
     }
 
@@ -27,6 +26,8 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
     }
 }
