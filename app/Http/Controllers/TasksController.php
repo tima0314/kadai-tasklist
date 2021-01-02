2371366c15052cbb5ded9d376fc11b2addbad4a7
tima0314 <tima314@gmail.com>
2021-01-02 08:01:07 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -34,6 +34,10 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+             // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
             // メッセージを作成
         $task = new Task;
         $task->content = $request->content;
@@ -70,6 +74,10 @@ class TasksController extends Controller
     // putまたはpatchでmessages/（任意のid）にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+             // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
                // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
         @include('commons.navbar')
 
         <div class="container">
+      {{-- エラーメッセージ --}}
+            @include('commons.error_tasks')
+            
             @yield('content')
         </div>
 
