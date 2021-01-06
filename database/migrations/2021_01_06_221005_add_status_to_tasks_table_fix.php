722c0b48c8ffa205477b355c2c9a997dbb6c3cbc
tima0314 <tima314@gmail.com>
2021-01-06 13:37:19 +0000

    migations fix

--- database/migrations/2021_01_06_221005_add_status_to_tasks_table_fix.php
+++ database/migrations/2021_01_06_221005_add_status_to_tasks_table_fix.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStatusToTasksTable extends Migration
+class AddStatusToTasksTableFix extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);
+            //
         });
     }
 
@@ -26,7 +26,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-             $table->dropColumn('status');
+            //
         });
     }
 }
