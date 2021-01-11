c646fcc341603f3c499e289bf78fc5b5360b02e1
tima0314 <tima314@gmail.com>
2021-01-11 02:47:05 +0000

    kadai_end

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -11,14 +11,23 @@ class TasksController extends Controller
     // getでmessages/にアクセスされた場合の「一覧表示処理」
     public function index()
     {
-              // メッセージ一覧を取得
-        $tasks = Task::all();
+       $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得
+            $tasks = $user->tasks()->orderBy('created_at', 'asc')->paginate(10);
 
-        // メッセージ一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+        }
+
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
     }
+    
 
     // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
     public function create()
@@ -43,6 +52,7 @@ class TasksController extends Controller
         $task = new Task;
         $task->status = $request->status; 
         $task->content = $request->content;
+        $task->user_id =\Auth::id();
         $task->save();
 
         // トップページへリダイレクトさせる
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+    
 
     <h1>タスク一覧</h1>
 
@@ -17,16 +17,16 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                     <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
+                    {{-- メッセージ作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
 
-   {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
